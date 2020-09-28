9a8228de328610e0b369aa32be4eaf571e2a9637
kushal.baldev <kushal.baldev@spec-india.com>
2020-09-28 16:33:18 +0530

    change username and password

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,10 +4,11 @@ export class User{
     
         @required({message:"This field is required"})
         @email({message:"Enter valid email"})
-        // @contains({ value: "aa@b.c",message:""})
+        @contains({ value: "admin@abc.com",message:""})
         public email:string;
 
         @required({message:"This field is required"})
+        @contains({ value: "password",message:""})
         public password:string;
         
  
