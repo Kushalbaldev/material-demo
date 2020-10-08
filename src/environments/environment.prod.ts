1ef826111542caa4b486997ab461ad472b921d27
kushal.baldev <kushal.baldev@spec-india.com>
2020-10-08 12:29:56 +0530

    added firebase properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDT30fxvXJEAV-f5-OX-KZZjwrdUIGRTKw',
+    authDomain: 'angular-9-app.firebaseapp.com',
+    databaseURL: 'https://angular-9-app.firebaseio.com',
+    projectId: 'angular-9-app',
+    storageBucket: 'angular-9-app.appspot.com',
+    messagingSenderId: '1000669920261',
+    appId: '1:1000669920261:web:90c26262db68c2a09df06c'
+
+  }
 };
